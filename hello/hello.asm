4daa55fb1432733ec786f5b52357c947a6a18d8b
Raisess <danilosantana456789@gmail.com>
2021-11-21 16:34:25 -0300

    hello: dynamic length get

--- hello/hello.asm
+++ hello/hello.asm
@@ -3,15 +3,16 @@ global _start
 section .data
 
 hello: db "hello, world!", 10
+  .len: equ $ - hello
 
 section .text
 _start:
-  mov rax, 1
+  mov rax, 1          ; stdout syscall
   mov rdi, 1
   mov rsi, hello
-  mov rdx, 14
+  mov rdx, hello.len
   syscall
 
-  mov rax, 60
+  mov rax, 60         ; exit syscall
   mov rdi, 0
   syscall
